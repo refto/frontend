40052b028a38fe161197f52202d11b24e0b6dda1
vovainside <vovainside@gmail.com>
2020-05-21 00:21:08 +0600

    add "welcome note"

--- pages/_.vue
+++ pages/_.vue
@@ -26,7 +26,20 @@
       <a-layout-content :style="{ marginTop: '64px' }">
         <div style="background-color: #ececec; padding: 20px;">
         <a-row :gutter="16" type="flex"  align="top" v-masonry  transition-duration="1s" item-selector=".masonryCard">
+          <a-col :xs="24" :sm="24" :md="12" :lg="8" :xl="6"   v-masonry-tile  class="masonryCard" v-if="this.selectedTopics.length < 1">
+            <a-card id="intro">
+              <p><b>Welcome to refto.dev</b> - a collection of awesome creations that is useful to software developers. The <a href="https://github.com/refto/data" target="_blank">data source is stored on github</a> and anyone is welcome to contribute.
+              </p>
+              <ul>
+                <li><a-icon type="github" /> <a href="https://github.com/refto/data" target="_blank">Data source</a></li>
+                <li><a-icon type="github" /> <a href="https://github.com/refto/frontend" target="_blank">Frontend source</a></li>
+                <li><a-icon type="github" /> <a href="https://github.com/refto/server" target="_blank">Server source</a></li>
+              </ul>
+            </a-card>
+          </a-col>
+
           <a-col :xs="24" :sm="24" :md="12" :lg="8" :xl="6"   v-masonry-tile  v-for="d in data" :key="d.token" class="masonryCard">
+
         <a-card>
           <a slot="extra" :href="editAddr(d.token, d.type)"  target="_blank"><a-icon type="edit"  title="Edit this" /></a>
           <a slot="title" :href="d.data.home_addr" :title="d.data.title"  target="_blank">
@@ -69,8 +82,8 @@
   </a-layout>
 </template>
 <script>
-    import GenericType from "../components/data-types/Generic.vue";
-    import BookType from "../components/data-types/Book.vue";
+    import GenericType from "../components/data-types/Generic";
+    import BookType from "../components/data-types/Book";
 
     export default {
         data() {
@@ -95,8 +108,8 @@
         },
 
         beforeMount() {
-            this.setSelectedTopicsFromPath()
-            this.loadData()
+          this.setSelectedTopicsFromPath()
+          this.loadData()
         },
 
         methods: {
@@ -124,6 +137,14 @@
 
                         this.topics = resp.topics
                         this.totalCount = resp.entities_count
+
+                        if (this.selectedTopics.length < 1) {
+                            // this.data = this.data.concat([{type: '', data:{
+                            //     title: "refto.dev",
+                            //     description: "refto.dev is a collection of anything that is useful to software developers. The data source is stored on github and anyone is welcome to contribute.",
+                            //     home_addr: "refto.dev is a collection of anything that is useful to software developers. The data source is stored on github and anyone is welcome to contribute.",
+                            // }}])
+                        }
                     })
                 } catch (e) {
                     this.$notify({
@@ -284,5 +305,40 @@
   #footerLinks li a:hover{
     color: white;
     text-decoration: none;
+  }
+  #intro {
+    font-size: 18px;
+    padding: 20px;
+    background: #168be5;
+    color: #c6d6ff;
+    box-shadow: none;
+  }
+  #intro a{
+    color: white;
+    text-decoration: underline;
+  }
+  #intro a:hover{
+    color: #02e8ff;
+    text-decoration: underline;
+  }
+  #intro  ul {
+    margin: 0;
+  }
+  #intro  ul li {
+    list-style: none;
+    font-size: 16px;
+  }
+
+  @media only screen and (max-width: 1000px) {
+    #logo {
+      width: 80px;
+      font-size: 30px;
+    }
+  }
+
+  @media only screen and (max-width: 700px) {
+    #logo {
+      display: none;
+    }
   }
 </style>
